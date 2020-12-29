99a5d6a849934d6dee77c22c54e7ca8d6069c0ca
Adam Nowland <acnowland@gmail.com>
2020-12-29 19:49:13 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  final_results = data.each_with_object({}) do |(key,value), final_hash|
+    value.each do |characteristic, pigeon|
+      pigeon.each do |pigeon_name|
+        if !final_hash[pigeon_name]
+          final_hash[pigeon_name] = {}
+        end
+        if !final_hash[pigeon_name][key]
+          final_hash[pigeon_name][key] = []
+        end
+        final_hash[pigeon_name][key].push(characteristic.to_s)
+      end
+    end
+  end
 end
