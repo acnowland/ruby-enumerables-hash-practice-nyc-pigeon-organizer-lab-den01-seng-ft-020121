c6e1a8d5d57b9ad482c5f7cf6c9197c7712e7146
Adam Nowland <acnowland@gmail.com>
2020-12-29 19:39:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  data.each_with_object({}) do |(key,value), final_hash|
+    value.each do |characteristic, pigeon|
+      pigeon.each do |pigeon_name|
+        if !final_hash[pigeon_name]
+          final_hash[pigeon_name] = {}
+          binding.pry
+        end
+        if !final_hash[pigeon_name][characteristic]
+          final_hash[pigeon_name] = characteristic
+        end
+      end
+    end
+  end
+  binding.pry
 end
